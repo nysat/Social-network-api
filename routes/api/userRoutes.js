3bd48e396e1e7e3e70c6a7b020a29e22a536ba7e
nysat <nysa-cere3@hotmail.com>
2023-11-05 03:38:08 -0500

    added the models and some code to server.js

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+//api/users
+
+
+//get all users
+
+
+//get a single user by _id and populated thought and friend data
+
+//post a new user 
+// // example data {
+//     "username": "lernantino",
+//     "email": "lernantino@gmail.com"
+//   }
+
+
+//put to update a user by its _id
+
+//delete to remove user by its _id
+
+//api/users/:userId/friends/:friendId
+
+// POST to add a new friend to a user's friend list
+
+// DELETE to remove a friend from a user's friend list
